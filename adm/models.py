d86498e7114e613e6c02a2333b0e7e32e3be5d91
Radu Mocanu <radu_mocanu2000@yahoo.com>
2019-05-07 20:26:05 +0300

    commit 2

--- adm/models.py
+++ adm/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Produs(models.Model):
+    nume = models.CharField(max_length=256)
+    pret = models.FloatField(default=1000.56)
+    descriere = models.CharField(max_length=4096, default='Nu exista descriere')
+
+    def __str__(self):
+        return self.nume
